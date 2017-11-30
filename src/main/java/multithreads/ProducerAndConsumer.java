5610b56eb3cde8630638040d5c3f5f0b2b92ba1e
Joseph <13714631658@163.com>
2017-11-30 15:37:02 +0800

    多线程 生产者消费者 更新

--- src/main/java/multithreads/ProducerAndConsumer.java
+++ src/main/java/multithreads/ProducerAndConsumer.java
@@ -7,12 +7,9 @@ public class ProducerAndConsumer {
 
     /**
      * 多线程，一个线程打印1-52，另一个线程打印字母A-Z
-     *  加 isProduce 是为了保证第一个输出的是Producer线程
      */
     static String answer = "12A34B56C78D910E1112F1314G1516H1718I1920J2122K2324L2526M2728N2930O3132P3334Q3536R3738S3940T4142U4344V4546W4748X4950Y5152Z";
     static StringBuilder builder = new StringBuilder();
-    static boolean isProduce = true;
-    static boolean isConsume = false;
     public static void main(String[] args){
         ProducerAndConsumer producerAndConsumer = new ProducerAndConsumer();
         producerAndConsumer.run();
@@ -28,7 +25,7 @@ public class ProducerAndConsumer {
         two.start();
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -44,10 +41,6 @@ public class ProducerAndConsumer {
         System.out.println("中了");
         System.out.println("result = "+result);
 
-        synchronized (builder){
-            //有个线程在wait
-            builder.notifyAll();
-        }
     }
 
     class Producer implements Runnable{
@@ -55,22 +48,17 @@ public class ProducerAndConsumer {
         int a = 1;
         @Override
         public void run() {
-            for(int i=0; i<26; ){
-                    synchronized (builder){
-                        System.out.println(this.getClass().getSimpleName());
-                        builder.append(a++);
-                        builder.append(a++);
-                        builder.notifyAll();
-                        i++;
-                        isProduce = false;
-                        isConsume = true;
-                        try {
-                            builder.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            for(int i=0; i<26; i++){
+                synchronized (builder){
+                    builder.append(a++);
+                    builder.append(a++);
+                    try {
+                        builder.notify();
+                        builder.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-
+                }
             }
         }
     }
@@ -80,23 +68,16 @@ public class ProducerAndConsumer {
         char a = 'A';
         @Override
         public void run() {
-            int i=0;
-            for(; i<26;){
-                    synchronized(builder){
-                        System.out.println(this.getClass().getSimpleName());
-                        builder.append(a);
-                        a++;
-                        builder.notifyAll();
-                        i++;
-                        isConsume = false;
-                        isProduce = true;
-                        try {
-                            builder.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+            for(int i=0; i<26; i++){
+                synchronized(builder){
+                    builder.append(a++);
+                    try {
+                        builder.notify();
+                        builder.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
-
+                }
             }
         }
     }
