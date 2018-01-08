037d2d0528562800ff0ac28c196c6138167fb6ca
joseph <josephcgh@163.com>
2018-01-08 15:12:22 +0800

    汉诺塔问题

--- src/main/java/TowerOfHanoi.java
+++ src/main/java/TowerOfHanoi.java
@@ -1,18 +1,38 @@
 package main.java;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Joseph at 2018/1/7 0007 18:57.
  *  汉诺塔问题
+ *  目标：将A的全部数字移动到C上。遵循以下规则：
+ *      1、每一个栈的数字严格遵循从小到大顺序，即升序
+ *      2、每次移动数字只能移动 1 个数字。
+ *
+ *  算法思路：
+ *      如果将 (1,2,3) 移动到 C 栈上，则必须先将(1,2)移动到B栈上，才能把3从A取出来放入到C中。
+ *      据此可以推理出，如果将(1,2)移动到B栈上，需将1移动到C栈，才能将2从A取出放入B.
+ *      设现有 N 个数字需要移动到C上，则欲将N移到C上，需先移动N-1到B，欲移N-1到B，需移N-2到C
+ *      可以无限细分下去，直到移动的数字为1可直接移动。
+ *      这符合一个递归的过程。
+ *      那么这就可以了吗？还忽略了1步，按上述步骤，为了将N移到C，N-1移到了B，N-2移到了C，实际算法执行的过程中
+ *      是从栈顶取元素即将N-2移到C，再将N-1移到B，可以发现只按上述步骤执行，只是满足将N-1移到B，N-1的确到了B但是N-2还在C上，
+ *      并没有考虑将N-2移回B上即N-1上，此时N移到C，发现N比N-2大不能直接放入，这就有问题。
+ *      所以每次移动数字后，需检查从该数字向上直到1的数字全部移动回当前栈上，为下一个数字N移动到C空出位置。
+ *      如果N-1向上N-2到1是多个数，这相当于将N-2个数从某个栈移到一个栈，与原问题性质一样的一个子问题，可以递归解决。
+ *
+ *      如何证明这个算法的正确性？
+ *      实际上述步骤将N个数字，交叉放置在另外2个栈上，每放置一个数字后将该数字上的所有数字拉回该栈它的上面，为原栈中下一个数字
+ *      移动做准备，当N被放置到目标栈后，N会发起递归，将N-1到1的全部数字放到目标栈N的上面，当程序结束时，问题便解决了。
+ *
+ *      算法时间复杂度：
+ *          直接看分析不出来，我调试程序发现，N=30时，运行时间为42秒，N每增加1，运行时间 * 2，
+ *          当 N=40时，运行时间为 42 * 2^10 秒。所以时间复杂度应为 2^N 指数级。
  */
 public class TowerOfHanoi {
 
     //栈A 4 3 2 1
-    static Stack A = new Stack("A","0",3,2,1);
+    static Stack A = new Stack("A","0");
     //栈B
     static Stack B = new Stack("B","1");
     //栈C
@@ -21,19 +41,19 @@ public class TowerOfHanoi {
     static Map<Integer,MapInfo> mapInfos = new HashMap<>();
 
     public static void main(String[] args) {
-        //初始化位置信息
-        mapInfos.put(1,new MapInfo(0));
-        mapInfos.put(2,new MapInfo(0));
-        mapInfos.put(3,new MapInfo(0));
+        for (int i=29; i>=1; --i){
+            A.push(i);
+            mapInfos.put(i,new MapInfo(0));
+        }
 
-        /**
-         * 目标：将A的全部数字移动到C上。遵循以下规则：
-         *  1、每一个栈的数字严格遵循从小到大顺序，即升序
-         *  2、每次移动数字只能移动 1 个数字。
-         */
-
-        //第1步，发现移动4个数字，数量大于1，递归
+        long start = System.currentTimeMillis();
+        //第1步，发现移动N个数字，数量大于1，递归
         recursionMove(0,A.stackSize()-1,A,C,B);
+
+        C.traversal();
+
+        System.out.println();
+        System.out.println((System.currentTimeMillis() - start) / 1000);
     }
 
     private static void recursionMove(int start,int end,Stack origin,Stack target,Stack standby){
@@ -48,20 +68,13 @@ public class TowerOfHanoi {
         }
 
         Integer top = origin.top();
-        if (top < target.littleSeeTop()){
+        if (target.canPush(top)){
             //该数字能直接放入目标栈
             target.push(top);
 
-            markMapInfo(top,target);
-        }
-        else {
-            //找到目标栈中待移动数字的下标，从它开始到栈顶的元素全部移动到另一个栈
-            int index = target.lastLessIndex(top);
-            //将目标栈的数字移到备栈，源栈此时当备栈
-            recursionMove(0,index,target,standby,origin);
-            //目标栈数字全部移动完毕，此时可将源栈数字移动到目标栈
-            target.push(top);
-
+            //只有入栈的情况才会引起栈的数字不合法（不符合升序），所以只需检查入栈的栈情况
+//            target.checkValid();
+//            showStack();
             markMapInfo(top,target);
         }
 
@@ -96,6 +109,16 @@ public class TowerOfHanoi {
         mapInfos.put(num,mapInfo);
     }
 
+    //打印3个栈的数字排列情况
+    private static void showStack(){
+        System.out.println("\n\nA栈：");
+        A.traversal();
+        System.out.println("\r\nB栈：");
+        B.traversal();
+        System.out.println("\r\nC栈：");
+        C.traversal();
+    }
+
     /** 栈存放元素 */
     private static class Stack{
 
@@ -105,14 +128,6 @@ public class TowerOfHanoi {
         private String name = "";
 
         List<Integer> elements = new ArrayList<>();
-
-        public Stack(Integer... elements){
-            if (null != elements && elements.length > 0){
-                for (Integer element : elements){
-                    this.elements.add(0,element);
-                }
-            }
-        }
 
         public Stack(String name,String no,Integer... elements){
             this.name = name;
@@ -134,7 +149,7 @@ public class TowerOfHanoi {
             if (elements.size() == 0){
                 return true;
             }
-            else if (elements.get(0) > element){
+            else if (littleSeeTop() > element){
                 return true;
             }
             return false;
@@ -175,6 +190,7 @@ public class TowerOfHanoi {
         public void traversal(){
             elements.forEach(element -> {
                 System.out.print(element + "  ");
+//                System.out.println(element);
             });
         }
 
@@ -188,21 +204,23 @@ public class TowerOfHanoi {
             return 0;
         }
 
-        public int getNo() {
-            return no;
+        //从栈底向栈顶遍历检查栈中数字的合法性
+        public void checkValid(){
+            for (int i = elements.size()-1; i > 0; i--){
+                if (elements.get(i) < elements.get(i-1)){
+                    throw new RuntimeException("栈 "+getName()+" 的数字不符合题目要求升序!!");
+                }
+            }
         }
 
-        public void setNo(int no) {
-            this.no = no;
+        public int getNo() {
+            return no;
         }
 
         public String getName() {
             return name;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
     }
 
     /** 节点数字的位置信息 */
