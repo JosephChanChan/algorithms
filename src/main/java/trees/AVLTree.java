b9cdb404a31addadb426f90c92d133314a839a7e
Joseph <13714631658@163.com>
2018-05-30 00:13:40 +0800

    AVLTree 平衡二叉树完善

--- src/main/java/trees/AVLTree.java
+++ src/main/java/trees/AVLTree.java
@@ -11,11 +11,25 @@ public class AVLTree {
 
     private AVLNode root;
 
+    /** tolerable difference height between left tree and right tree */
+    private static final int MAX_DIFF_HEIGHT = 1;
+
+
     public <T extends Comparable> void insert(T beInsert){
         if (null != beInsert){
             root = insert(beInsert, root);
         }
     }
+
+    /**
+     * Insert a T into the tree.
+     * Return a sub tree node when every recursion.
+     *
+     * @param beInsert T
+     * @param avlNode tree node
+     * @param <T> T
+     * @return node
+     */
     private <T extends Comparable> AVLNode insert(T beInsert, AVLNode avlNode){
         if (null == avlNode){
             return new AVLNode(beInsert);
@@ -30,63 +44,112 @@ public class AVLTree {
         else {
             // beInsert equals someone, we needn't it
         }
+
         // calculate height of node
         avlNode.height = Math.abs(calculateHeight(avlNode.left) - calculateHeight(avlNode.right)) + 1;
+
+        // UnBalance maybe occur after insert, do balance fixing it
+        return balance(avlNode);
+    }
+
+    public <T extends Comparable> void remove(T removed){
+        if (
+                null != removed &&
+                null != root
+           )
+        {
+            root = remove(removed, root);
+        }
+    }
+    private <T extends Comparable> AVLNode remove(T removed, AVLNode avlNode){
+        if (null != avlNode){
+            int result = removed.compareTo(avlNode.element);
+            if ( result < 0 ){
+                avlNode.left = remove(removed, avlNode.left);
+            }
+            else if ( result > 0 ){
+                avlNode.right = remove(removed, avlNode.right);
+            }
+            else {
+                // got the node. lets remove it
+
+                if (null == avlNode.left && null == avlNode.right){
+                    avlNode = null;
+                }
+                // only have left
+                else if (null == avlNode.right){
+                    avlNode = avlNode.left;
+                }
+                // only have right
+                else if (null == avlNode.left){
+                    avlNode = avlNode.right;
+                }
+                // have left and right
+                else {
+                    /*
+                        we should find a node which greater than all left child
+                        and less than all right child. that is most depth left of firstly right child.
+                        After the removed was instead of minimum, we must remove the minimum.
+                        A recursion process would be called which are same as before.
+                     */
+                    AVLNode minimum = findMin(avlNode.right);
+                    avlNode.element = minimum.element;
+                    avlNode.right = remove(minimum.element, avlNode.right);
+                }
+
+                // UnBalance maybe occur after remove, do balance fixing it
+                return balance(avlNode);
+            }
+        }
         return avlNode;
     }
 
-    public <T extends Comparable> AVLNode remove(T removed){
-        if (null == removed){
-            return null;
+    private AVLNode balance(AVLNode avlNode){
+        // check if unbalance is occur
+        int diff = Math.abs(calculateHeight(avlNode.left) - calculateHeight(avlNode.right));
+        if (diff > MAX_DIFF_HEIGHT){
+            // then we must fixing it, but we have to confirm which status of unbalance
+
+            /*
+                unbalance situation:
+                    1, the new node has been inserted at left tree's left side (LL)
+                    2, the new node has been inserted at left tree's right side (LR)
+                    3, the new node has been inserted at right tree's left side (RL)
+                    4, the new node has been inserted at right tree's right side (RR)
+                we have 4 algorithm to handle corresponding situation.
+                Mapping relationship:
+                LL -> rightRotate
+                RR -> leftRotate
+                LR -> leftRotateThenRightRotate
+                RL -> rightRotateThenLeftRotate
+             */
         }
-        if (null == root){
-            return null;
-        }
-        return remove(removed, root);
-    }
-    private <T extends Comparable> AVLNode remove(T removed, AVLNode avlNode){
-        int result = removed.compareTo(avlNode.element);
-        if ( result < 0 ){
-            return remove(removed, avlNode.left);
-        }
-        else if ( result > 0 ){
-            return remove(removed, avlNode.right);
-        }
-        else {
-            if (null == avlNode.left && null == avlNode.right){
-                avlNode = null;
-            }
-            // only have left
-            else if (null == avlNode.right){
-                avlNode = avlNode.left;
-            }
-            // only have right
-            else if (null == avlNode.left){
-                avlNode = avlNode.right;
-            }
-            // have left and right
-            else {
-                /*
-                    we should find a node which greater than all left child
-                    and less than all right child. that is most depth left of firstly right child
-                 */
-                AVLNode right = avlNode.right;
-                AVLNode min = findMin(avlNode.right);
-                avlNode = min;
-                remove(min.element, right);
-            }
-        }
+
         return avlNode;
     }
 
     private AVLNode findMin(AVLNode avlNode){
-        // if it have left child, the most depth left is minimum
-        if (null != avlNode.left){
-            return findMin(avlNode.left);
+        AVLNode minimum = null;
+        if (null != avlNode){
+            // if it have left child, the most depth left is minimum
+            while (null != avlNode.left){
+                avlNode = avlNode.left;
+            }
+            minimum = avlNode;
         }
-        else {
-            return avlNode;
+        return minimum;
+    }
+
+    private AVLNode findMax(AVLNode avlNode){
+        AVLNode maximum = null;
+        if (null != avlNode){
+            // the most depth right is maximum
+            while (null != avlNode.right){
+                avlNode = avlNode.right;
+            }
+            maximum = avlNode;
         }
+        return maximum;
     }
 
     public <T extends Comparable> boolean contains(T beSearch, AVLNode avlNode){
