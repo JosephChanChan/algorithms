d11dfeec4837fbd574d70bb4c2774d79e65eef78
joseph <josephcgh@163.com>
2018-06-28 18:20:32 +0800

    update

--- src/main/java/string/KMPAlgorithm.java
+++ src/main/java/string/KMPAlgorithm.java
@@ -4,9 +4,11 @@ package main.java.string;
  * Created by Joseph on 2017/11/14.
  *
  * KMP算法:
- *  当2个字符串匹配时，穷举法步骤是两个指针i,j分别指向原串和模式串，当2个字符相同时指针分别往后移动，但若出现字符不匹配时，
- *  i指针回到原来的i+1位置，j指针则回到模式串的第0位置，继续匹配，这样做的效率太低，O(n*m) 并且浪费了原有一部分字符串匹配的事实（因为i和j指针都同时移动了n位)
- *  KMP算法就是解决只回退j指针，不回退i指针，大大降低时间复杂度，O(n+m)，利用了原有一部分字符串匹配的事实，将j指针尽量回退到一个有效位置
+ *  当2个字符串匹配时，穷举法步骤是两个指针 i, j 分别指向原串和模式串，当 2 个字符相同时指针分别往后移动，但若出现字符不匹配时，
+ *  i 指针回到原来的 i+1 位置，j 指针则回到模式串的第 0 位置，继续匹配，这样做的效率太低，O(n*m)
+ *  并且浪费了原有一部分字符串匹配的事实（因为 i 和 j 指针都同时移动了 n 位)
+ *  KMP算法就是解决只回退 j 指针，不回退 i 指针，大大降低时间复杂度，O(n+m)，利用了原有一部分字符串匹配的事实，
+ *  将 j 指针尽量回退到一个有效位置
  *
  * 关于next数组求解算法可看:
  *   @see_https://www.cnblogs.com/tangzhengyue/p/4315393.html
@@ -24,24 +26,24 @@ public class KMPAlgorithm {
 
         int s = -1;
         for (int i = 0, j = 0; i < strText.length && j < strTarget.length; ){
-            if(strText[i] == strTarget[j]){
-                if(s == -1){
+            if (strText[i] == strTarget[j]){
+                if (s == -1){
                     s = i;
-                    if(j != 0){
+                    if (j != 0){
                        s = i - j;
                     }
                 }
                 ++i;
-                if(j++ == strTarget.length-1){
+                if (j++ == strTarget.length-1){
                     System.out.println("target串匹配成功，下标："+s+" "+(s+strTarget.length-1));
                 }
             }
             else {
                 s = -1;
-                //当某个字符不匹配了，i不回退，只回退j，j回退位置由next数组得出
+                // 当某个字符不匹配了，i不回退，只回退j，j回退位置由next数组得出
                 j = next[j];
-                if(j == -1){
-                    //j是-1模式串不能再向前回溯了，原串向后移动
+                if (j == -1){
+                    // j是-1模式串不能再向前回溯了，原串向后移动
                     i++; j++;
                 }
             }
@@ -56,11 +58,11 @@ public class KMPAlgorithm {
         int[] next = new int[target.length];
         next[0] = -1;
 
-        //假设前j位已求出，求j+1位
-        for (; j<target.length-1; ){
-            if(k == -1 || target[k] == target[j]){
-                //如果j+1已失配情况下，k+1等于j+1的话，即使回退到k+1匹配还是失败，所以干脆回到next[k+1]
-                if(target[k+1] == target[j+1]){
+        // 假设前j位已求出，求j+1位
+        for ( ; j < target.length - 1; ){
+            if (k == -1 || target[k] == target[j]){
+                // 如果j+1已失配情况下，k+1等于j+1的话，即使回退到k+1匹配还是失败，所以干脆回到next[k+1]
+                if (target[k+1] == target[j+1]){
                     next[++j] = next[++k];
                 }
                 else {
