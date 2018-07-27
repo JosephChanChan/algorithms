59948aedca6bd05d141ceb391a3bcdfe15c11e70
joseph <josephcgh@163.com>
2018-07-27 19:01:47 +0800

    贪心算法 - 活动安排问题(通过51nod测试)

--- src/main/java/greedy/algorithm/ActivePlan.java
+++ src/main/java/greedy/algorithm/ActivePlan.java
@@ -1,12 +1,10 @@
 package greedy.algorithm;
 
+import sort.QuickSort;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * Question Description:
@@ -37,6 +35,22 @@ import java.util.TreeSet;
  *   for 2: 最大可能为 N-1
  *    for 3: 不做优化的情况下，最大为 N
  *
+ *  这种思路看起来没问题吧？抱歉，是错的... 错在和第2种策略是类似的。
+ *
+ *  下面讲正确的贪心策略：
+ *  结束时间越早的活动优先。这个策略是有效的. 贪心策略的正确性证明可以通过数学归纳法证明.
+ *  设 S[] 集合为按照活动结束时间升序排列的原集合。
+ *  A[] 为最优解，同样也是按照活动结束时间升序排列的集合。
+ *  P[] 为按照 “结束时间越早的活动优先” 的贪心策略选择出来的集合。
+ *  f(i) 表示某个活动结束时间.
+ *  现在考虑 A[0] = P[0] 吗？ 如果 A[0] = P[0] OK，很好。
+ *  如果 A[0] != P[0] ，那么 f(P[0]) <= f(A[0]).(一开始我在这里卡了很久, 不明白为什么肯定小于等于?)
+ *  P[0] 按照我们的贪心策略从S中选出来的，如果出现 Ai 和 Pi f(Ai) < f(Pi) 那么按照策略肯定选的是Ai，
+ *  所以 f(P[0]) > f(A[0]) 有悖贪心策略，不存在的。
+ *  那么 P[0] 就可以替代 A[0] ，P[0]也一定小于A[1]...A[m]的开始时间。
+ *  这样就与设定矛盾，A[] 就不是最优解了！ 所以A[0] != P[0]的情况不存在！
+ *  依次类推，证明了 P[0] = A[0] 可以扩展 P[k] 与 A[k] 的关系。同样的证明方法。
+ *  至此，我们证明了按照策略选出来的P[]与最优解A[]是相同的, 贪心策略得证.
  *
  * created by Joseph
  * at 2018/7/25 17:49
@@ -45,19 +59,19 @@ public class ActivePlan {
 
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in),1 << 16);
-        int activeCount = 0;
-        List<Active> activeList = new ArrayList<>();
+        int activeCount = 0, maxAvailableCount = 0;
+        Active[] activeArray = null;
         try {
             activeCount = Integer.parseInt(reader.readLine());
             String[] params ;
+            activeArray = new Active[activeCount];
             for (int i = 0; i < activeCount; i++){
                 params = reader.readLine().split(" ");
                 Active active = new Active();
-                active.setSerialNo(i);
                 active.setStartTime(Integer.parseInt(params[0]));
                 active.setEndTime(Integer.parseInt(params[1]));
                 active.setTime(Math.abs(active.getStartTime() - active.getEndTime()));
-                activeList.add(active);
+                activeArray[i] = active;
             }
         }
         catch (IOException e) {
@@ -65,73 +79,27 @@ public class ActivePlan {
         }
 
         /*
-            伪代码:
-              for 1: 枚举每一个活动开头
-                  Ai = Active[i];// 以这一个活动开头找到最优的一条活动链
-                  for 2: 对于这条活动链长度是未知的，Aj ... An 最大可能为N，所以要遍历完活动数组...
-                      for 3: 从剩下的N-j个活动中找下一个开始时间大于前一个结束时间的活动, 并选出时间最短的.
+            贪心策略：每一次选择结束时间越早的活动且与前面活动相容的。
+            为此我们需要先将活动排序
          */
-        int maxActiveCount = 0;
+        QuickSort.doQuickSort(activeArray, 0, activeArray.length - 1);
 
-        for (int i = 0; i < activeCount; i++){
-            Set<Integer> serialSet = new TreeSet<>();
+        int perEndTime = Integer.MIN_VALUE;
 
-            Active active = activeList.get(i);
-            serialSet.add(active.getSerialNo());
-            int preEndTime = active.getEndTime();
-
-            for (int j = 0; j < activeCount; j++){
-                Active min = null;
-
-                for (int k = 0; k < activeCount; k++){
-                    Active temp = activeList.get(k);
-                    if (
-                            temp.getStartTime() >= preEndTime &&
-                            !serialSet.contains(temp.getSerialNo())
-                       )
-                    {
-                        if (null == min){
-                            min = temp;
-                        }
-                        else if (temp.getTime() < min.getTime()){
-                            min = temp;
-                        }
-                    }
-                }
-
-                // 在活动数组中已经找不到合适的下一个活动了
-                if (null == min){
-                    break;
-                }
-                // 找到一个时间最短且可以衔接上上一个活动的活动
-                else {
-                    serialSet.add(min.getSerialNo());
-                    preEndTime = min.getEndTime();
-                }
-            }
-
-            if (serialSet.size() > maxActiveCount){
-                maxActiveCount = serialSet.size();
+        for (int k = 0; k < activeCount; k++){
+            if (activeArray[k].startTime >= perEndTime){
+                maxAvailableCount++;
+                perEndTime = activeArray[k].endTime;
             }
         }
 
-        System.out.println(maxActiveCount);
+        System.out.println(maxAvailableCount);
     }
 
-
-    static class Active {
-        private int serialNo;
+    static class Active implements Comparable {
         private int startTime;
         private int endTime;
         private int time;
-
-        public int getSerialNo() {
-            return serialNo;
-        }
-
-        public void setSerialNo(int serialNo) {
-            this.serialNo = serialNo;
-        }
 
         public int getStartTime() {
             return startTime;
@@ -156,5 +124,20 @@ public class ActivePlan {
         public void setTime(int time) {
             this.time = time;
         }
+
+        @Override
+        public int compareTo(Object o) {
+            Active active = (Active) o;
+            if (endTime > active.getEndTime()){
+                return 1;
+            }
+            else if (endTime == active.getEndTime()){
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        }
     }
+
 }
