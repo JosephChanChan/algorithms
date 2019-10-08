1208510ad45226b1bb7fb48ba7f24664b74d1893
Joseph <JosephCGH@163.com>
2019-10-08 21:31:16 +0800

    update BFS

--- src/main/java/graphs/BFSAlgorithm.java
+++ src/main/java/graphs/BFSAlgorithm.java
@@ -1,53 +1,32 @@
 package graphs;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 /**
  * 广度优先搜索
- * 读一个文本文件，包含了一个矩阵，该矩阵是图的转化(该文件在本地文件中, 表现形式为邻接矩阵, 请 Google 或 bing 了解)
- * 构造好邻接矩阵后, 便可以运行程序.
- * 该程序阅读不友好, 有空改善下.
- * <p>
+ * 广搜整体思想是，从图中任意一个节点出发，访问到该节点后（可以对其做操作），搜索该节点链接的临节点，
+ * 将临界点放入队列中，这视为一个循环操作完成。
+ * 下一步从队列中取出节点，访问 -> 搜索临节点 -> 放入队列。
+ * 循环至队列为空，至此图中所有节点被访问到，算法结束。
+ *
  * Created by Joseph on 2017/6/10.
  */
 public class BFSAlgorithm {
 
-    private static final int MATRIX_MAX = 6;
+    private static final int MATRIX_MAX = 7;
 
-    static int[][] matrix = new int[MATRIX_MAX][MATRIX_MAX];
     private static int[] queue = new int[MATRIX_MAX];
-    static int[] visited = new int[MATRIX_MAX];
+    private static int[] visited = new int[MATRIX_MAX];
+    private static int[][] matrix = {
+            {0, 1, 1, 0, 0, 1, 1},
+            {1, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1, 0},
+            {0, 0, 0, 1, 0, 1, 1},
+            {1, 0, 0, 1, 1, 0, 0},
+            {1, 0, 0, 0, 1, 0, 0}
+    };
 
-    public static void main(String[] args) throws FileNotFoundException {
-        /*FileInputStream inputStream = new FileInputStream("F:\\matrix.txt");
-        int i = 0, j = 0, k = 0;
-        try {
-            while ((i=inputStream.read())!=-1){
-                if((char)i=='0' || (char)i=='1'){
-                    matrix[j][k] = i;
-                    k++;
-                }else if((char)i=='\n'){
-                    j++;
-                    k=0;
-                }
-            }
-
-//            for(i=0; i<MATRIX_MAX; i++){
-//                for(j=0; j<MATRIX_MAX; j++){
-//                    System.out.print((char)matrix[i][j]+"  ");
-//                }
-//            }
-            bfsSearch(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        int h = 123;
-        int i = 1;
-        System.out.println(i << 16);
-        System.out.println(h >>> 16);
+    public static void main(String[] args) {
+        bfsSearch(0);
     }
 
     private static void bfsSearch(int start) {
@@ -55,14 +34,14 @@ public class BFSAlgorithm {
         queue[tail++] = start;
         while (head != tail) {
             int t = queue[head++];
-            System.out.println(t + 1);
+            System.out.println(t);
             visited[t] = 1;
-            //查找该节点的临节点
+            // 查找该节点的临节点
             for (int i = 0; i < MATRIX_MAX; i++) {
-                if ((char) matrix[t][i] == '1') {
-                    //如果没被访问
+                if (matrix[t][i] == 1) {
+                    // 如果没被访问
                     if (visited[i] == 0) {
-                        //入队
+                        // 入队
                         queue[tail++] = i;
                         visited[i] = 1;
                     }
