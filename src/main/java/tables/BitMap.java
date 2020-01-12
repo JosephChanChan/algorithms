7a1b0630d2823bd3cd2893f805255db8a2803a3a
Joseph <JosephCGH@163.com>
2020-01-12 19:54:51 +0800

    位图算法和位图算法的测试。

--- src/main/java/tables/BitMap.java
+++ src/main/java/tables/BitMap.java
@@ -1,8 +1,87 @@
 package tables;
 
 /**
+ * 位图算法是一种逻辑存在的数据结构，其特殊的特点可以用来处理大数据量的场景，
+ * 例如对大数据的数字查找、数字去重。Java中boolean貌似最小占一个byte，最小的数据类型又是byte。
+ * 所以可以用byte[]模拟位图数据结构。
+ *
  * @author Joseph
  * @since 2020-01-12 17:15
  */
 public class BitMap {
+
+    private byte[] bitMap ;
+
+
+
+    /**
+     * 设置一个可以容纳 1 ~ num 容量的位图。
+     *
+     * @param num 数字
+     */
+    public BitMap(int num) {
+        // 数组长度最大限制在 Int.max
+        bitMap = new byte[(num >> 3) + 1];
+    }
+
+    /**
+     * 将数字k放进位图中。
+     * 常数时间。
+     *
+     * @param k 数字
+     */
+    public void put(int k) {
+        int index = getIndex(k);
+        checkRange(k, index);
+
+        bitMap[index] &= (1 << getPosition(k));
+    }
+
+    /**
+     * 删除数字k。
+     * 常数时间。
+     *
+     * @param k 数字
+     */
+    public void remove(int k) {
+        int index = getIndex(k);
+        checkRange(k, index);
+
+        bitMap[index] &= ~(1 << getPosition(k));
+    }
+
+    /**
+     * 位图中是否存在数字k。
+     * 常数时间。
+     *
+     * @param k 数字
+     * @return 0/1
+     */
+    public boolean contains(int k) {
+        int index = getIndex(k);
+        if (index > bitMap.length) return false;
+
+        // 为了避免k是负数，判断不等于0
+        return (bitMap[index] | ~(1 << getPosition(k))) != 0;
+    }
+
+
+
+    /* ----------------------------------- internal method ----------------------------------- */
+
+    /** 计算 k 在第几个byte */
+    private int getIndex(int k) {
+        return k >> 3;
+    }
+
+    /** 计算 k 在byte中的位置，k & 7 <-> k % 8 */
+    private int getPosition(int k) {
+        return k & 0x7;
+    }
+
+    private void checkRange(int k, int index) {
+        if (index > bitMap.length) {
+            throw new IllegalArgumentException("The number k are exceed the bound of bitmap!");
+        }
+    }
 }
---
+++ src/main/test/BitMapTest.java
@@ -0,0 +1,48 @@
+import tables.BitMap;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @author Joseph
+ * @since 2020-01-12 19:08
+ */
+public class BitMapTest {
+
+
+    public static void main(String[] args) {
+        int[] arr = {
+                9, 6, 5, 4, 18, 76, 28, 99, 72, 27,
+                99, 64, 28, 39, 71, 28, 47, 28, 85, 94
+        };
+
+        Map<Integer, Integer> occurrenceNum = new HashMap<>(arr.length);
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+            occurrenceNum.put(arr[i], 0);
+        }
+
+        BitMap bitMap = new BitMap(max);
+
+        for (int j = 0; j < arr.length; j++) {
+            if (bitMap.contains(arr[j])) {
+                occurrenceNum.put(arr[j], occurrenceNum.get(arr[j]) + 1);
+            }
+            else {
+                bitMap.put(arr[j]);
+            }
+        }
+
+        Iterator<Map.Entry<Integer, Integer>> iterator = occurrenceNum.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            System.out.println(entry.getKey() + " ::: " + entry.getValue());
+        }
+    }
+
+
+
+}
