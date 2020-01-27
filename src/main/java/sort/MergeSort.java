9f9a7577ad834ce81835daacc84ceb111d2412b9
Joseph <JosephCGH@163.com>
2020-01-27 21:29:38 +0800

    1、优化合并排序。2、AC leetcode 95 medium

--- src/main/java/sort/MergeSort.java
+++ src/main/java/sort/MergeSort.java
@@ -10,8 +10,8 @@ package sort;
  *  将左右 2 部分有序的小数组合并成一个有序的稍大的数组。当递归栈依次结束时，每次都有小数组被合并成有序的大数组。
  *  算法结束时，数组将有序。
  *
- * 时间复杂度：
- *  O(N logN)
+ * 时间复杂度：O(N logN)
+ * 空间复杂度：O(N)
  *
  * created by Joseph
  * at 2018/8/31 0:26
@@ -21,75 +21,69 @@ public class MergeSort {
     static int[] array = {16, 5, 47, 8, 35, 56, 31, 27, 46, 3, 28, 6, 13};
 
     public static void main(String[] args) {
-        mergeSort(array);
+        array = mergeSort(array);
 
         int result = CheckSortedArr.checkIntAsc(array);
         System.out.println(result);
     }
 
-    public static void mergeSort(int[] beSorted){
-        int[] tempArray = new int[beSorted.length];
-        mergeSort(beSorted, tempArray, 0, beSorted.length - 1);
+    public static int[] mergeSort(int[] beSorted){
+        return mergeSort(beSorted, 0, beSorted.length - 1);
     }
 
     /**
      * 分治递归算法，将大数组不断二等分分割小数组，直至小数组有序的，然后合并左右 2 部分子数组。
      *
-     * @param beSorted 原数组
-     * @param tempArray 临时数组
-     * @param left 开始位
-     * @param right 结束位
+     * @param beSorted  原数组
+     * @param left      开始位
+     * @param right     结束位
      */
-    private static void mergeSort(int[] beSorted, int[] tempArray, int left, int right){
+    private static int[] mergeSort(int[] beSorted, int left, int right){
         // means the sub array has elements to be sorted
         if (left < right){
             int center = (left + right) >> 1;
             // continue partition sub array until the array is sorted
-            mergeSort(beSorted, tempArray, left, center);
-            mergeSort(beSorted, tempArray, center + 1, right);
+            int[] leftSorted = mergeSort(beSorted, left, center);
+            int[] rightSorted = mergeSort(beSorted, center + 1, right);
             // both sub array is sorted, just merge them
-            merge(beSorted, tempArray, left, center + 1, right);
+            return merge(leftSorted, rightSorted);
         }
+        return new int[]{beSorted[left]};
     }
 
     /**
      * 左指针会和右指针比较，较小者存入临时数组中，然后指针继续移动下一个。
-     * 直到算法将所有元素合并完，再将临时数组中元素拷贝回原数组对应位置中。
-     * 此时原数组对应的位置即 leftPos ~ rightEnd 这部分元素变为有序的。
+     * 直到算法将所有元素合并完。
      *
-     * @param original 原数组
-     * @param tempArray 临时数组
-     * @param leftPos 左指针
-     * @param rightPos 右指针
-     * @param rightEnd 右指针结束位置
+     * @param leftSorted    左侧有序数组
+     * @param rightSorted   右侧有序数组
+     * @return int[]        新的有序数组
      */
-    private static void merge(int[] original, int[] tempArray, int leftPos, int rightPos, int rightEnd){
-        int tempPos = leftPos,
-            leftEnd = rightPos - 1,
-            countElement = rightEnd - leftPos + 1;
+    private static int[] merge(int[] leftSorted, int[] rightSorted){
+        int leftPos = 0, rightPos = 0, pos = 0;
+        int[] sorted = new int[leftSorted.length + rightSorted.length];
 
-        while (leftPos <= leftEnd && rightPos <= rightEnd){
-            if (original[leftPos] >= original[rightPos]){
-                tempArray[tempPos++] = original[rightPos++];
+        while (leftPos < leftSorted.length &&
+                rightPos < rightSorted.length) {
+            if (leftSorted[leftPos] > rightSorted[rightPos]) {
+                sorted[pos++] = rightSorted[rightPos++];
             }
             else {
-                tempArray[tempPos++] = original[leftPos++];
+                sorted[pos++] = leftSorted[leftPos++];
             }
         }
 
-        // maybe one sub array has elements to be merge
-        // is left side sub array ?
-        while (leftPos <= leftEnd){
-            tempArray[tempPos++] = original[leftPos++];
+        if (leftPos == leftSorted.length) {
+            while (rightPos < rightSorted.length) {
+                sorted[pos++] = rightSorted[rightPos++];
+            }
         }
-        // or right side ?
-        while (rightPos <= rightEnd){
-            tempArray[tempPos++] = original[rightPos++];
+        else {
+            while (leftPos < leftSorted.length) {
+                sorted[pos++] = leftSorted[leftPos++];
+            }
         }
 
-        // the temp array are sorted and move back into original
-        for (int k = 0; k < countElement; k++, rightEnd--){
-            original[rightEnd] = tempArray[rightEnd];
-        }
+        return sorted;
     }
 }
---
+++ src/main/java/trees/ValidBinarySearchTree.java
@@ -0,0 +1,121 @@
+package trees;
+
+/**
+ * @author Joseph
+ * @since 2020-01-27 19:38
+ *
+ * leetcode 98 medium
+ *
+ * Question Description:
+ *  Given a binary tree, determine if it is a valid binary search tree (BST).
+ * Assume a BST is defined as follows:
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * Example 1:
+ *     2
+ *    / \
+ *   1   3
+ * Input: [2,1,3]
+ * Output: true
+ * Example 2:
+ *     5
+ *    / \
+ *   1   4
+ *      / \
+ *     3   6
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ *
+ * Analysis:
+ *  分治递归。
+ *  1、divide 以当前节点为根，左右子树划分。
+ *  2、conquer 对当前节点的左右子树深搜判断是否合法，合法的话继续以左右子节点为根做深搜。
+ *  3、combine 合并左右子树结果做判断。
+ *
+ *  时间复杂度：O(N^2)
+ *  空间复杂度：O(1)
+ */
+public class ValidBinarySearchTree {
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode one = new TreeNode(1);
+        TreeNode four = new TreeNode(4);
+        TreeNode three = new TreeNode(3);
+        TreeNode six = new TreeNode(6);
+        root.left = one;
+        one.right = four;
+        four.left = three;
+        root.right = six;
+
+        ValidBinarySearchTree valid = new ValidBinarySearchTree();
+        boolean validBST = valid.isValidBST(root);
+        System.out.println(validBST);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return recursivelyCheckNode(root);
+    }
+
+    private boolean recursivelyCheckNode(TreeNode node) {
+        if (null == node) return true;
+
+        boolean leftValid = true,
+                rightValid = true;
+
+        if (null != node.left) {
+            // 深搜左子树
+            leftValid = depthCheck(node, node.left, true);
+        }
+        if (null != node.right) {
+            // 深搜右子树
+            rightValid = depthCheck(node, node.right, false);
+        }
+
+        if (leftValid && rightValid) {
+            // 遍历下一个左节点
+            if (null != node.left) {
+                leftValid = recursivelyCheckNode(node.left);
+            }
+            // 遍历下一个右节点
+            if (null != node.right) {
+                rightValid = recursivelyCheckNode(node.right);
+            }
+            return leftValid && rightValid;
+        }
+
+        return false;
+    }
+
+    private boolean depthCheck(TreeNode ancestor, TreeNode node, boolean leftSide) {
+        if (null == node) return true;
+        boolean valid = leftSide ? leftCheck(ancestor, node) : rightCheck(ancestor, node);
+        if (!valid) return false;
+        // 深搜左边
+        valid = depthCheck(ancestor, node.left, leftSide);
+        if (!valid) return false;
+        // 深搜右边
+        return depthCheck(ancestor, node.right, leftSide);
+    }
+
+    private boolean leftCheck(TreeNode ancestor, TreeNode node) {
+        return ancestor.val > node.val;
+    }
+
+    private boolean rightCheck(TreeNode ancestor, TreeNode node) {
+        return ancestor.val < node.val;
+    }
+
+
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+}
