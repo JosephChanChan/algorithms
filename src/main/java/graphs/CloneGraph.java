e0b387f74f28a56d9cf182df1bf65bd60be249a8
Joseph <JosephCGH@163.com>
2020-08-30 17:17:20 +0800

    optimize lc 133

--- src/main/java/graphs/CloneGraph.java
+++ src/main/java/graphs/CloneGraph.java
@@ -9,7 +9,7 @@ import java.util.*;
  *  参见 lc 133
  *
  * Analysis:
- * 时间复杂度：O(n^2)
+ * 时间复杂度：O(n+m)
  * 空间复杂度：O(n)
  *
  * @author Joseph
@@ -33,7 +33,7 @@ public class CloneGraph {
         return newGraphs.get(node.val);
     }
 
-    /* 28ms faster than 59% */
+    // 28ms faster than 59%
     private void bfs() {
         while (!queue.isEmpty()) {
             Node node = queue.poll();
@@ -63,6 +63,63 @@ public class CloneGraph {
             newGraphs.put(newNode.val, newNode);
         }
         return newNode;
+    }
+
+    /*public Node cloneGraph(Node node) {
+        *//*
+            第二种思路，分步来，先克隆点再克隆边，时间复杂度一样。AC 28ms
+            1.clone nodes
+            2.clone edges
+        *//*
+
+        Map<Integer, Node> newGraphs = new HashMap<>(32);
+        Map<Integer, Node> oldGraphs = new HashMap<>(32);
+
+        cloneNodes(node, newGraphs, oldGraphs);
+
+        cloneEdges(newGraphs, oldGraphs);
+
+        return newGraphs.get(node.val);
+    }*/
+
+    private void cloneEdges(Map<Integer, Node> newGraphs, Map<Integer, Node> oldGraphs) {
+        for (Map.Entry<Integer, Node> entry : newGraphs.entrySet()) {
+            Node n = entry.getValue();
+            Node old = oldGraphs.get(n.val);
+
+            List<Node> neighbors = n.neighbors;
+            List<Node> oldNeighbors = old.neighbors;
+            for (int i = 0; i < oldNeighbors.size(); i++) {
+                Node neighbor = newGraphs.get(oldNeighbors.get(i).val);
+                neighbors.add(neighbor);
+            }
+        }
+    }
+
+    private void cloneNodes(Node root, Map<Integer, Node> newGraphs, Map<Integer, Node> oldGraphs) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node old = queue.poll();
+            oldGraphs.put(old.val, old);
+
+            getNewNode(old, newGraphs);
+            List<Node> neighbors = old.neighbors;
+            for (int i = 0; i < neighbors.size(); i++) {
+                if (newGraphs.containsKey(neighbors.get(i).val)) continue;
+                queue.add(neighbors.get(i));
+            }
+        }
+    }
+
+    private Node getNewNode(Node old, Map<Integer, Node> newGraphs) {
+        Node n = newGraphs.get(old.val);
+        if (null == n) {
+            n = new Node(old.val);
+            newGraphs.put(old.val, n);
+        }
+        return n;
     }
 
     static class Node {
