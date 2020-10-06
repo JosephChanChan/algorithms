ef9d1d2d52db91e096a69c68c6f995a98498451e
Joseph <JosephCGH@163.com>
2020-10-06 10:54:29 +0800

    lc 368

--- src/main/java/dynamic/programming/EditDistance.java
+++ src/main/java/dynamic/programming/EditDistance.java
@@ -1,105 +1,15 @@
 package dynamic.programming;
 
-import java.io.*;
+import java.util.*;
 
 /**
- * 给定两个字符串S和T，对于T我们允许三种操作：
- *  (1) 在任意位置添加任意字符
- *  (2) 删除存在的任意字符
- *  (3) 修改任意字符
- *  问最少操作多少次可以把字符串T变成S？
- *  例如： S＝  “ABCF”   T = “DBFG”
- *  那么我们可以
- *  (1) 把D改为A
- *  (2) 删掉G
- *  (3) 加入C
- *  所以答案是3。
+ * leetcode 72 hard
  *
- *  输入
- *  第1行：字符串a(a的长度 <= 1000)。
- *  第2行：字符串b(b的长度 <= 1000)。
- *  输出
- *  输出a和b的编辑距离
- *  输入示例
- *  kitten
- *  sitting
- *  输出示例
- *  3
- *
- *  假如要把S字符串转化为T字符串
- *  子问题：    设 f(i,j)为字符串S前i位和字符串T前j位最小的编辑距离。
- *  如果S[i] == T[j] 则求最少的编辑距离 f(i,j) = f(i-1,j-1)
- *  如果S[i] != T[j]  有3种情况可做
- *  1，把T[j]加入到S[i]前面，这样T[j]==S[i+1] 就只需要计算 f(i,j-1)的编辑距离 + 1( 1为添加T[j]的操作)
- *  2，既然不等于，就删除S[i]，这样就只需要计算 f(i-1,j)的编辑距离 + 1( 1为删除S[i]的操作)
- *  3，把S[i]修改成T[j]即可，这样只需要计算 f(i-1,j-1)的编辑距离 + 1( 1为修改S[i]的操作)
- *  最优子结构： 对于S[i] != T[j] 选上面3种情况最少的情况。
- *  递推公式:   f(i,j) = min{ f(i-1,j-1)+same(i,j) , f(i,j-1)+1 , f(i-1,j)+1 }
- *
- *  http://blog.csdn.net/qq_34552886/article/details/72556242
- *
- * Created by Joseph on 2017/8/8.
+ * Analysis:
+ * 时间复杂度：
+ * 空间复杂度：
  */
 public class EditDistance {
 
-    static char[] s = null,t=null;
-    static int[][] arr = null;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in),1<<16);
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
-        int i = 0,j = 0;
-        String a = reader.readLine();
-        s = a.toCharArray();
-        String b = reader.readLine();
-        t = b.toCharArray();
-        arr = new int[s.length+1][t.length+1];
-
-        for(; i<=s.length; i++){
-            arr[i][0] = i;
-        }
-
-        for(; j<=t.length; j++){
-            arr[0][j] = j;
-        }
-
-        for(i=0; i<s.length; i++){
-            for(j=0; j<t.length; j++){
-                //f(i,j) = min{ f(i-1,j-1)+same(i,j) , f(i,j-1)+1 , f(i-1,j)+1 }
-                int val = chooseMin(i+1, j+1);
-                arr[i+1][j+1] = val;
-            }
-        }
-
-//            for(i=0; i<s.length+1; i++){
-//                for(j=0; j<t.length+1; j++){
-//                    System.out.print(arr[i][j]+" ");
-//                }
-//                System.out.println();
-//            }
-
-
-        writer.write(arr[s.length][t.length]+"\r\n");
-        writer.flush();
-
-    }
-
-    private static int chooseMin(int i,int j){
-        int val = 0;
-        if(arr[i-1][j-1] + same(i-1,j-1) < arr[i][j-1] + 1){
-            val = arr[i-1][j-1] + same(i-1,j-1);
-        }else{
-            val = arr[i][j-1] + 1;
-        }
-
-        if(val > arr[i-1][j]+1){
-            val = arr[i-1][j]+1;
-        }
-
-        return val;
-    }
-
-    private static int same(int i,int j){
-        return s[i] == t[j] ? 0 : 1;
-    }
 }
---
+++ src/main/java/dynamic/programming/LargestDivisibleSubset.java
@@ -0,0 +1,75 @@
+package dynamic.programming;
+
+import java.util.*;
+
+/**
+ * leetcode 368 medium
+ *
+ * Analysis:
+ *  先排序，使得数组按升序 a1<a2<a3<...<an
+ * 这样数组任意两个元素都是不同的，所以任意 j < i都有Sj < Si
+ * 且如果Sj Si存在倍数关系，则肯定是 Si % Sj=0 且 Sj % Si=Sj
+ *
+ * 假设存在一种最大的整除序列P
+ * P1,P2...Pk，这个序列满足：P1<P2<...<Pk，最后一个元素Pk去掉后剩下的P1~Pk-1仍是原序列中能挑出的最大整除序列。
+ * Pk和Pk-1显然满足：Pk-1<Pk && Pk % Pk-1 = 0
+ * 那么Pk是否和序列中其它元素存在倍数关系？
+ * Pk-1和其它元素存在倍数关系，Pj < Pk-1 && Pk-1/Pj=x，Pk/pk-1=y
+ * 则 Pk-1=Pj*x，Pk=pk-1*y，Pk=Pj*x*y
+ * 所以是Pk只要和Pk-1比较就行。
+ *
+ * f(i)是以Ai结尾的最大整除序列的长度
+ * f(i)=max{f(j) | j < i && Aj < Ai && Ai % Aj=0} + 1
+ * 边界：f(0)=0, f(1)=1
+ *
+ * 时间复杂度：O(n^2)
+ * 空间复杂度：O(n)
+ *
+ * @author Joseph
+ * @since 2020-10-06 10:51
+ */
+public class LargestDivisibleSubset {
+
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        List<Integer> ans = new LinkedList<>();
+        int n = nums.length;
+        if (n == 0) return ans;
+        if (n == 1) {
+            ans.add(nums[0]);
+            return ans;
+        }
+
+        Arrays.sort(nums);
+
+        int[] f = new int[n];
+        // 保存当前状态从哪个前置状态转移过来的
+        int[] track = new int[n];
+        f[0] = 1;
+        track[0] = 0;
+        for (int i = 1; i < n; i++) {
+            int max = 0, maxIdx = i;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && f[j] > max) {
+                    max = f[j];
+                    maxIdx = j;
+                }
+            }
+            f[i] = max + 1;
+            track[i] = maxIdx;
+        }
+        int max = 0, maxIdx = 0;
+        for (int i = 0; i < n; i++) {
+            if (f[i] > max) {
+                max = f[i];
+                maxIdx = i;
+            }
+        }
+        // 根据track[]选出最优序列的元素
+        while (true) {
+            ans.add(nums[maxIdx]);
+            if (maxIdx == track[maxIdx]) break;
+            maxIdx = track[maxIdx];
+        }
+        return ans;
+    }
+}
