a23eb438a369ab90bdbb94c78e5f823bb589ea81
Joseph <JosephCGH@163.com>
2021-09-25 16:27:30 +0800

    QuickSort template

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -1,144 +1,57 @@
 package sort;
 
-import java.util.Random;
-
 /**
+ * lc 912 medium
+ *
  * 快速排序，如同名称一样，对于一般实现，它是比较快的一种排序算法。
- * 平均算法时间 O(N logN)，上界 O(n^2)。
+ * 平均算法时间 O(N*logN)，上界 O(n^2)。
+ * 快速排序有很多个实现版本，选择这种理解记忆就好，这种是令狐冲推荐的比较好的版本。
  *
  * 算法思想：
- *  和归并排序相似，都是采用递归分而治之的算法。
- *  选出一个基准数（一般选择数组头元素），用变量记录下来。
- *  2个指针分别指向左开头与右结尾，右指针向左遍历，只留下大于等于基准数的元素，直到遇到一个小于基准数的元素停止。
- *  接下来左指针向右遍历，只留下小于等于基准数的元素，直到遇到一个大于基准数的元素停止。
- *  将右指针与左指针的元素互换，重复此过程直到 左指针 = 右指针。此时将基准数覆盖至此位置。算法完成一轮排序。
- *  此时的数组将会是这样的情况，基准数左边全部小于等于它，右边则全部大于等于它。
- *  以基准数为中间界，递归左部分和右部分，重复以上步骤。递归结束后数组将有序。
+ * 该版本的快排是将数组分为左右两部分，左边<=p，右边>=p，没有将p放在中间分割整体，
+ * 而是以l r交错的地方分割递归。
+ * partition之后，l和r必定是交错的，start~r部分是<=p，l~end部分是>=p，也就是左边<=右边，所以整体上看已经有序。
+ * 递归处理 start~r
+ * 递归处理 l~end
  *
- *  PS：本实例的过程与上述稍做了点改动，在右指针与左指针遍历部分，但是整体思想与上述一致。
+ * 有3个点需要注意：
+ * 1.中轴p的选择，p不建议选开头/结尾，如果数组本身有序，会导致一趟partition后，递归处理n-1规模的问题，构造出n^2最坏情况
+ * 2.对于l r遇到等于p的元素处理，遇到等于p的元素也交换，这样可以使得左右两边均等长，使每次递归处理接近一半的规模
+ * 3.while循环中 l<=r 不是 l<r，避免左右递归出现交集情况，交集情况会出现stack over
  *
  * Created by Joseph on 2017/6/10.
  */
 public class QuickSort {
 
-    static int[] arr = {5,8,3,7,1,6,4,9,2};
-
     public static void main(String[] args){
-        /*Random random = new Random();
-        for(int j=0; j<100; j++){
-            arr[j] = random.nextInt(100);
-        }
-
-        doQuickSort(arr,0,arr.length-1);
-
-        int flag = CheckSortedArr.checkAsc(arr);
-
-        if(flag != -1) {
-            System.out.println("排序有误!");
-        }
-        else {
-            System.out.println("排序正确");
-        }
-
-        for(int i : arr){
-            System.out.print(i+" ");
-        }*/
-
-        /*doQuickSortDesc(arr,0, arr.length-1);*/
-        doQuickSort(arr, 0, arr.length - 1);
-
-        /*int flag = CheckSortedArr.checkIntAsc(arr);
-        if (flag != -1){
-            System.out.println("数组排序有误! "+flag);
-        }
-        else {
-            System.out.println("数组排序成功!");
-        }*/
-
-        for (Integer integer : arr){
+        int[] a = new int[]{4,3,9,7,6,5};
+        quickSort(0, a.length-1, a);
+        for (Integer integer : a){
             System.out.print(integer + " ");
         }
     }
 
-    /** 针对基本数据类型 int 服务 */
-    public static void doQuickSort(int[] arr, int left, int right) {
-        if(left >= right) return;
-        int i = quickSort(arr, left, left, right);
+    static void quickSort(int start, int end, int[] a) {
+        // start > end is wrong and start == end only one that needn't sort
+        if (start >= end) return;
+        // notice1 pivot chosen mid is better
+        // p is value not index cause the value that p pointer may change
+        int l = start, r = end, p = a[(l+r)>>1];
 
-        doQuickSort(arr, left,i - 1);
-        doQuickSort(arr, i + 1, right);
-    }
-    private static int quickSort(int[] arr, int pivot, int left, int right) {
-        while (left < right){
-            while (left < right && arr[right] >= arr[pivot]){
-                right--;
+        // notice2 l <= r not l < r
+        while (l <= r) {
+            // notice 3 a[l] < p not <=
+            while (l <= r && a[l] < p) l++;
+            while (l <= r && a[r] > p) r--;
+            if (l <= r) {
+                int t = a[l];
+                a[l] = a[r];
+                a[r] = t;
+                l++; r--;
             }
-            while (left < right && arr[left] <= arr[pivot]){
-                left++;
-            }
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
         }
-        int temp = arr[left];
-        arr[left] = arr[pivot];
-        arr[pivot] = temp;
-        return left;
-    }
-
-    /** 针对基本数据类型 int 服务. 降序排序 */
-    public static void doQuickSortDesc(int[] arr, int left, int right){
-        if(left >= right){
-            return;
-        }
-
-        int base = arr[left];
-
-        int i = quickSortDesc(arr, base, left, right);
-
-        doQuickSortDesc(arr,left,i-1);
-        doQuickSortDesc(arr,i+1,right);
-    }
-    private static int quickSortDesc(int[] arr, int base, int left, int right){
-        while (left < right){
-            while (left < right && arr[right] <= base){
-                right--;
-            }
-            arr[left] = arr[right];
-            while (left < right && arr[left] >= base){
-                left++;
-            }
-            arr[right] = arr[left];
-        }
-        arr[left] = base;
-        return left;
-    }
-
-    /** 针对实现 Comparable 可排序接口的泛型服务 */
-    public static <T extends Comparable<? super T>> void doQuickSort(T[] arr,int left,int right){
-        if(left >= right){
-            return;
-        }
-
-        T base = arr[left];
-
-        int i = quickSort(arr, base, left, right);
-
-        doQuickSort(arr,left,i-1);
-        doQuickSort(arr,i+1,right);
-    }
-    private static <T extends Comparable<? super T>> int quickSort(T[] arr, T base, int left, int right){
-        while (left < right){
-            while (left < right && arr[right].compareTo(base) >= 0){
-                right--;
-            }
-            arr[left] = arr[right];
-            while (left < right && arr[left].compareTo(base) <= 0){
-                left++;
-            }
-            arr[right] = arr[left];
-        }
-        arr[left] = base;
-        return left;
+        // r must > l here
+        quickSort(start, r, a);
+        quickSort(l, end, a);
     }
 }
