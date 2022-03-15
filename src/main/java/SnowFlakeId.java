5b58e4dd72042e8ec5c245f3c5998d6608c130b7
Joseph <JosephCGH@163.com>
2022-03-15 18:42:50 +0800

    优化雪花算法和测试类，经实战校验

--- src/main/java/SnowFlakeId.java
+++ src/main/java/SnowFlakeId.java
@@ -1,111 +1,118 @@
 
-import kits.GetNetworkAddress;
-
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * 雪花算法，总长 64 位，最高位是0代表整个id为正数 + 41位时间戳 + 10位机器码（可细分数据中心、机器码） + 12位序列号。
- *
- * @author Joseph
- * @since  2019/5/31 14:30
+ * 雪花算法，总长 64 位，最高位是0代表整个id为正数 + 42位时间戳 + 8位机器码 + 13位序列号
+ * 42位时间长度可用139年
+ * 8位机器码可部署256台实例
+ * 13位序列号一毫秒内可生成8192个id
+ * 可根据需要调整各部分位数长度，适合自己的业务
  */
 public class SnowFlakeId {
 
-    private static int SEQUENCE = 1;
-    private static long LAST_TIMESTAMP ;
-    private static final long START_UP_TIMESTAMP ;
+    /**
+     * 毫秒内序列号
+     */
+    private int sequence = 0;
 
-    // 机器位数
-    private static final int MACHINE_BIT = 10;
-    // 序列位数
-    private static final int SEQUENCE_BIT = 12;
+    /**
+     * 机器id
+     */
+    private final int workerId ;
 
-    private static final int MACHINE_OFFSET = SEQUENCE_BIT;
-    private static final int TIMESTAMP_OFFSET = MACHINE_BIT + SEQUENCE_BIT;
+    /**
+     * 纪元从2022-01-01零点开始
+     */
+    private final long startUpTime = 1640966400000L;
 
-    // ~(-1 << MACHINE_BIT) 等价于 -1 ^ (-1 << MACHINE_BIT)
-    private final static int MACHINE_MASK = ~(-1 << MACHINE_BIT);
+    /**
+     * 上一个id的时间戳
+     */
+    private long lastTime = startUpTime;
 
-    // TOP_ZERO = 0111 1111 ... 1111    用来去最高位的数，使最高位保持为0
-    private static final int TOP_ZERO = Integer.MAX_VALUE;
+    /**
+     * 机器位数
+     */
+    private final int machineBit = 8;
 
-    static {
-        START_UP_TIMESTAMP = LocalDateTime.parse("2020-06-07 00:00:00",
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")).toInstant(ZoneOffset.of("+8")).toEpochMilli();
-        LAST_TIMESTAMP = START_UP_TIMESTAMP;
+    /**
+     * 序列位数
+     */
+    private final int sequenceBit = 13;
+
+    /**
+     * 机器码偏移量 12位
+     */
+    private final int machineOffset = sequenceBit;
+
+    /**
+     * 时间戳偏移量 18位
+     */
+    private final int timestampOffset = machineBit + sequenceBit;
+
+    /**
+     * 机器掩码 0000 0000 ... 111 111
+     * ~(-1 << MACHINE_BIT) 等价于 -1 ^ (-1 << MACHINE_BIT)
+     */
+    private final int machineMask = ~(-1 << machineBit);
+
+    /**
+     * 0111 1111 ... 1111 64位，用来去最高位的数，使最高位保持为0
+     */
+    private final long topMask = Long.MAX_VALUE;
+
+
+
+    public SnowFlakeId(int workerId) {
+        this.workerId = workerId;
     }
 
-    private SnowFlakeId() {
-    }
-
-    public static long nextSnowFlakeId(long machineNum) {
-        long snow_flake ;
-
-        // 只取低10位数值
-        machineNum = machineNum & MACHINE_MASK;
-
-        synchronized (SnowFlakeId.class) {
-            long currentTimeStamp = System.currentTimeMillis();
-            // 如果当前是同一毫秒内并发请求
-            if (currentTimeStamp == LAST_TIMESTAMP) {
-                // 超过12位长度的序列号，等待下一毫秒
-                if ((++SEQUENCE >> SEQUENCE_BIT) > 0) {
-                    SEQUENCE = 1;
-                    currentTimeStamp = wait2NextMillis();
-                }
-            }
-            // 新的一毫秒，序列号从 1 开始
-            else {
-                SEQUENCE = 1;
-            }
-            snow_flake = (currentTimeStamp - START_UP_TIMESTAMP) << TIMESTAMP_OFFSET | machineNum << MACHINE_OFFSET | SEQUENCE & TOP_ZERO;
-            LAST_TIMESTAMP = currentTimeStamp;
+    public synchronized long nextId() {
+        long nowMillis = System.currentTimeMillis();
+        if (nowMillis < lastTime) {
+            // 时钟回拨
+            nowMillis = handleTimeBack(nowMillis, lastTime);
         }
-        return snow_flake;
+        if (nowMillis > lastTime) {
+            // 新的一毫秒，序列号从 0 开始
+            sequence = 0;
+        }
+        else {
+            // 超过12位长度的序列号，等待下一毫秒
+            if ((sequence >> sequenceBit) > 0) {
+                sequence = 0;
+                nowMillis = wait2NextMillis();
+            }
+        }
+        sequence++;
+        lastTime = nowMillis;
+        return (((nowMillis - startUpTime) << timestampOffset) | ((workerId & machineMask) << machineOffset) | sequence) & topMask;
     }
 
-    private static long wait2NextMillis() {
+    private long wait2NextMillis() {
         long currentTime = System.currentTimeMillis();
-        while (currentTime <= LAST_TIMESTAMP) {
+        while (currentTime <= lastTime) {
             currentTime = System.currentTimeMillis();
         }
         return currentTime;
     }
 
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        /*long l1 = DateKit.addYears(DateKit.now(), -2).getTime();
-        long diff = System.currentTimeMillis() - l1;
-        System.out.println(diff);
-        System.out.println(patch(Long.toBinaryString(diff)));
-
-        System.out.println(Long.toBinaryString(-1));
-
-        System.out.println(diff << TIMESTAMP_OFFSET);
-        System.out.println(Long.toBinaryString(diff << TIMESTAMP_OFFSET));*/
-
-        String mac = GetNetworkAddress.getAddress("mac");
-        System.out.println(mac + " and it's hashcode is " + mac.hashCode());
-        int h = mac.hashCode(), k = 10000000, i ;
-        Set<Long> ids = new HashSet<>(k);
-        for (int j = 0; j < 10; j++) {
-            for (i = 0; i < k; i++) {
-                long snowFlakeId = nextSnowFlakeId(h);
-                boolean nx = ids.add(snowFlakeId);
-                if (!nx) {
-                    System.out.println(i);
-                    System.out.println("Wrong! snowFlakeId already exists!");
-                    break;
-                }
-                /*System.out.println(snowFlakeId);*/
-            }
-            ids.clear();
+    private long handleTimeBack(long now, long last) {
+        if (last - now > 5) {
+            throw new RuntimeException(
+                    String.format("Clock moved backwards.  Refusing to generate id for diff %d milliseconds", last - now));
         }
+        try {
+            // 时钟回拨在5ms内，忍一忍
+            wait((last - now) << 1);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        now = System.currentTimeMillis();
+        if (now < last) {
+            throw new RuntimeException(
+                    String.format("Clock moved backwards.  Refusing to generate id for diff %d milliseconds", last - now));
+        }
+        return now;
     }
 
 }
--- src/main/test/SnowFlakeIdTest.java
+++ src/main/test/SnowFlakeIdTest.java
@@ -1,202 +1,58 @@
-import java.io.*;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Joseph
  * @since 2019/6/4 10:31
  */
 public class SnowFlakeIdTest {
+    SnowFlakeId snowFlakeId = new SnowFlakeId(10);
+    CountDownLatch c = new CountDownLatch(36);
+    AtomicInteger p = new AtomicInteger(0);
+    List<Long>[] total = new List[36];
 
-    private int count = 3000000;
-    private volatile boolean flag = true;
+    int w = 36, maxW = w;
 
-    private final Object remains = new Object();
-    private Map<Long, Object> vessel = new ConcurrentHashMap<>();
-    private Map<Long, Object> vessel2 = new ConcurrentHashMap<>();
+    ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(
+            w, maxW, 10, TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(36));
 
-    private static List<Object[]> queueList = new ArrayList<>();
-
-    private ThreadLocal<Queue<Long>> localQueue = new ThreadLocal<>();
-    private ThreadLocal<ReentrantReadWriteLock> localLock = new ThreadLocal<>();
-
-    private CountDownLatch latch = new CountDownLatch(7);
-
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 11; i++) {
-            long time = System.currentTimeMillis();
-            SnowFlakeIdTest test = new SnowFlakeIdTest();
-
-            test.doConcurrentTask();
-            test.latch.await();
-            long tail = System.currentTimeMillis();
-            System.out.println((tail - time) / 1000);
-            System.out.println("All workers finished jobs! ");
-        }
-    }
-
-    private void doConcurrentTask() {
-        // 3个线程，每个线程运行 500w 次获取雪花id，看是否有重复
-        for (int i = 0; i < 3; i++) {
-            new Thread(new SpadeSnowWorker()).start();
-        }
-        // 3个线程，每个线程运行 500w 次获取雪花id，看是否有重复
-        for (int i = 0; i < 3; i++) {
-            new Thread(new SpadeSnowWorker2()).start();
-        }
-        // 异步消费雪花id，都写进文件中，后面如果有冲突，可以查这个文件
-        new Thread(new FileWorker()).start();
-    }
-
-    private void storeId(Long snowFlakeId) {
-        Queue<Long> list = localQueue.get();
-        if (null == list) {
-            list = new LinkedList<>();
-            localQueue.set(list);
-
-            // 默认非公平模式读写锁
-            ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-            localLock.set(readWriteLock);
-
-            Object[] kits = new Object[2];
-            kits[0] = readWriteLock;// 操作队列前先获取锁
-            kits[1] = list;// 队列
-            queueList.add(kits);
-        }
-
-        // 往队列写数据时要注意并发，先获取写锁
-        ReentrantReadWriteLock readWriteLock = localLock.get();
-        try {
-            readWriteLock.writeLock().lock();
-            list.offer(snowFlakeId);
-        }
-        finally {
-            readWriteLock.writeLock().unlock();
-        }
-    }
-
-    private class SpadeSnowWorker implements Runnable {
-
-        @Override
-        public void run() {
-            Object old ;
-            long snowFlakeId ;
-            for (int i = 0; i < count && flag; i++) {
-                snowFlakeId = SnowFlakeId.nextSnowFlakeId(12);
-                old = vessel.put(snowFlakeId, remains);
-                if (null != old) {
-                    System.out.println("SnowFlakeId already exists in vessel! it is " + snowFlakeId);
-                    flag = false;
-                }
-                storeId(snowFlakeId);
-            }
-            latch.countDown();
-        }
-    }
-
-    private class SpadeSnowWorker2 implements Runnable {
-
-        @Override
-        public void run() {
-            Object old ;
-            long snowFlakeId ;
-            for (int i = 0; i < count && flag; i++) {
-                snowFlakeId = SnowFlakeId.nextSnowFlakeId(13);
-                old = vessel2.put(snowFlakeId, remains);
-                if (null != old) {
-                    System.out.println("SnowFlakeId already exists in vessel2! it is " + snowFlakeId);
-                    flag = false;
-                }
-                storeId(snowFlakeId);
-            }
-            latch.countDown();
-        }
-    }
-
-    private class FileWorker implements Runnable {
-
-        File file ;
-        static final String filePath = "D:\\SnowFlakeIdList.txt";
-        BufferedWriter bufferedWriter = null;
-
-        public FileWorker() {
-            file = new File(filePath);
-            try {
-                bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-            }
-            catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void run() {
-            int queueCount = queueList.size();
-            try {
-                // 当前还有写线程在工作，还得继续读
-                while (1 < latch.getCount()) {
-                    pollingQueue(queueCount);
-                }
-                // 所有写线程完成后，再轮询一遍队列，防止读线程睡眠时写线程写了数据
-                pollingQueue(queueCount);
-            }
-            finally {
+    void test() {
+        poolExecutor.prestartAllCoreThreads();
+        for (int j = 0; j < maxW; j++) {
+            poolExecutor.execute(() -> {
+                LinkedList<Long> list = new LinkedList<>();
                 try {
-                    if (null != bufferedWriter) {
-                        bufferedWriter.flush();
-                        bufferedWriter.close();
-                    }
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            latch.countDown();
-        }
-
-        private void pollingQueue(int queueCount) {
-            Object[] kits ;
-            Queue<Long> queue ;
-            ReentrantReadWriteLock readWriteLock ;
-
-            // 不断轮询每个队列，写进本地文件
-            for (int i = 0; i < queueCount; i++) {
-                kits = queueList.get(i);
-                queue = (Queue<Long>) kits[1];
-                readWriteLock = (ReentrantReadWriteLock) kits[0];
-
-                try {
-                    readWriteLock.readLock().lock();
-                    while (queue.size() > 0) {
-                        Long snowFlakeId = queue.poll();// 从队列头出队
-                        if (null == snowFlakeId) continue;
-                        write2File(snowFlakeId);
+                    System.out.println("Thread="+Thread.currentThread().getName()+" begin");
+                    for (int i = 0; i < 200000; i++) {
+                        list.add(snowFlakeId.nextId());
                     }
                 }
                 finally {
-                    readWriteLock.readLock().unlock();
+                    System.out.println("Thread="+Thread.currentThread().getName()+" done");
+                    c.countDown();
+                    total[p.getAndIncrement()] = list;
+                    System.out.println("Thread="+Thread.currentThread().getName()+" exit, list size="+list.size());
+                }
+            });
+        }
+        try {
+            System.out.println("Thread="+Thread.currentThread().getName()+" wait");
+            c.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        poolExecutor.shutdownNow();
+        System.out.println("Thread="+Thread.currentThread().getName()+" distincting");
+        Set<Long> distinct = new HashSet<>();
+        for (List<Long> list : total) {
+            for (Long l : list) {
+                if (!distinct.add(l)) {
+                    throw new RuntimeException("重复的雪花id="+l);
                 }
             }
-
-            // 本次轮询完毕，先睡10ms避免与写线程过度争抢锁和cpu
-            try {
-                Thread.sleep(10);
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
-
-        private void write2File(Long snow) {
-            try {
-                bufferedWriter.write(String.valueOf(snow));
-                bufferedWriter.newLine();
-            }
-            catch (IOException ioEx) {
-                ioEx.printStackTrace();
-            }
-        }
+        System.out.println("main done, no repeated id");
     }
 }
